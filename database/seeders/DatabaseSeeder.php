5e31a3b8ab03d8a3b3bcab1bd2e997420dfd7868
retvain <retvain@gmail.com>
2021-03-25 16:37:26 +0500

    step 6, make seeds for categories and users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call(UserTableSeeder::class);
+        $this->call(BlogCategoriesTableSeeder::class);
+        \App\Models\User::factory(50)->create();
     }
 }
